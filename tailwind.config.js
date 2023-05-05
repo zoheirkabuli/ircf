2a4213441f7a61acfdcb7f30730476fbf237249a
zoheirkabuli <zoheyrk@gmail.com>
2023-05-05 15:31:47 +0330

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/tw-elements/dist/js/**/*.js',
   ],
   theme: {
     extend: {
@@ -15,4 +16,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+};
